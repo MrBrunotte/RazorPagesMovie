60c730a6057c0e5c47257e739d3ffa7be8b5e317
mrbrunotte <mrbrunotte@gmail.com>
2020-10-13 09:58:58 +0200

    Add project files.

---
+++ RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        // I can remove the RazorPagesMovie.public DbSet<Movie> Movie { get; set; }
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        // ID is required by the database for the primary key.
+        public int ID { get; set; }
+        public string Title { get; set; }
+        // Data Annotation. Only date is displayed, not time
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
